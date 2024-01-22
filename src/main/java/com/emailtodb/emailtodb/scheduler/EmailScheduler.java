12302d464c07a388f011add4cf27a1f284f7bfbe
Gursimran <gurmsi1060@outlook.com>
2024-01-22 21:35:19 +0530

    cleaned the code

--- src/main/java/com/emailtodb/emailtodb/scheduler/EmailScheduler.java
+++ src/main/java/com/emailtodb/emailtodb/scheduler/EmailScheduler.java
@@ -14,15 +14,10 @@ public class EmailScheduler {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailScheduler.class);
 
-
     @Autowired
     private EmailSummaryService emailService;
 
-//    @Autowired
-//    private FileDownloadService fileDownloadService;
-
     //@Scheduled(cron = "0 0 */12 * * *") // Runs every 12 hours, adjust as needed
-    //@Scheduled(fixedDelay = 60*60*12*1000, initialDelay = 1000)
     @Scheduled(fixedDelay = 12 * 60 * 60 * 1000, initialDelay = 60 * 1000)
     // Runs every 12 hours, with delay of 1 minute after application start up (to allow for email fetching)
     public void fetchEmailsRegularly() {
@@ -40,14 +35,20 @@ public class EmailScheduler {
         }
     }
 
-//    Runs every 12 hours, with delay of 5 minutes after application start up (to allow for email fetching)
-//    @Scheduled(fixedDelay = 12 * 60 * 1000 * 60, initialDelay = 1000 * 60 * 5)
-//    public void savingEmailAttachmentsRegularly() {
-//        try {
-//            fileDownloadService.downloadAllFiles("output");
-//            logger.info("Fetched and saved attachments successfully");
-//        } catch (Exception e) {
-//            logger.error("Error occurred while fetching and saving attachments: " + e.getMessage());
-//        }
-//    }
+    /*
+    @Autowired
+    private FileDownloadService fileDownloadService;
+
+    //Runs every 12 hours, with delay of 5 minutes after application start up (to allow for email fetching)
+    @Scheduled(fixedDelay = 12 * 60 * 1000 * 60, initialDelay = 1000 * 60 * 5)
+    public void savingEmailAttachmentsRegularly() {
+        try {
+            fileDownloadService.downloadAllFiles("output");
+            logger.info("Fetched and saved attachments successfully");
+        } catch (Exception e) {
+            logger.error("Error occurred while fetching and saving attachments: " + e.getMessage());
+        }
+    }
+    */
+
 }
